b8d7d6cf02fffd6bec9ed868d4d176467a177f18
elAsTeru <ta0e9lrmuai54@gmail.com>
2023-05-17 06:16:19 +0900

    "Hello Haskell"(文字)を描画

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
-import Lib
+import Graphics.Gloss
+
+------------------------
+-- Displayの設定
+------------------------
+wndW, wndH :: Num a => a
+wndW = 896
+wndH = 1024
+
+wnd :: Display
+wnd = InWindow "FROGGER" (wndW, wndH) (100, 100)
 
 main :: IO ()
-main = someFunc
+main = display wnd white(translate (-150) (-10) . scale 0.5 0.5 $ text "Hello Haskell")
